3a9a71b3e93b4891adfb79c5b9c6f92ef2423357
Ajay Rao <ajayrao895@gmail.com>
2023-06-09 20:07:16 +0530

    Added api endpoint to fetch all the expense groups of a user

--- src/app/api/expenseGroups/route.ts
+++ src/app/api/expenseGroups/route.ts
@@ -1,29 +1,53 @@
 import { NextResponse } from "next/server";
 import { StatusCodes } from "http-status-codes";
-import { create } from "@/db/DALs/expenseGroupsDAL";
+import { create, getByUserId } from "@/db/DALs/expenseGroupsDAL";
 
 export async function POST(request: Request) {
   try {
-    
     let expenseGroup = await request.json();
 
     const userDataString = request.headers.get("userData");
 
     if (userDataString) {
-        const userData = JSON.parse(userDataString);
+      const userData = JSON.parse(userDataString);
 
-        expenseGroup = {
-            ...expenseGroup,
-            createdBy: userData._id
-        };
+      expenseGroup = {
+        ...expenseGroup,
+        createdBy: userData._id,
+      };
     }
 
     const createdExpenseGroup = await create(expenseGroup);
-    
-    return NextResponse.json({ success: true, createdExpenseGroup });
 
+    return NextResponse.json({ success: true, createdExpenseGroup });
   } catch (error) {
     console.log("error while creating ", error);
+    return NextResponse.json(
+      { success: false },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    const userDataString = request.headers.get("userData");
+
+    if (userDataString) {
+      const userData = JSON.parse(userDataString);
+
+      const expenseGroups = await getByUserId(userData._id);
+
+      return NextResponse.json({
+        success: true,
+        expenseGroups,
+      });
+
+    } else {
+      throw new Error("user data not found in request");
+    }
+  } catch (error) {
+    console.log("error while fetching expense groups", error);
     return NextResponse.json(
       { success: false },
       { status: StatusCodes.INTERNAL_SERVER_ERROR }
--- src/db/DALs/expenseGroupsDAL.ts
+++ src/db/DALs/expenseGroupsDAL.ts
@@ -1,14 +1,34 @@
 import dbConnect from "../dbConnect";
 import ExpenseGroups from "../models/expenseGroups";
+import { Types } from "mongoose";
+
 
 type ExpenseGroup = {
     admin: string,
     name: string,
     description: string,
-    users: Array<string>
+    otherUsers: Array<string>
 };
 
 export const create = async (expenseGroup: ExpenseGroup) => {
     await dbConnect();
     return ExpenseGroups.create(expenseGroup);
+};
+
+/**
+ * Get all the expenseGroups which has the given userId as either the admin or one of the 'otherUsers'
+ * @param userId string
+ */
+export const getByUserId = async (userId: string) => {
+    await dbConnect();
+    return ExpenseGroups.find({
+        $or: [
+            {
+                admin: new Types.ObjectId(userId)
+            },
+            {
+                otherUsers: new Types.ObjectId(userId)
+            }
+        ]
+    });
 };
