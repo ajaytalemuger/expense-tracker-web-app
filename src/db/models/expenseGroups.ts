2358e9985aa5608c899a97befb0d9848f0e80370
Ajay Rao <ajayrao895@gmail.com>
2023-06-09 20:06:35 +0530

    Updated expense groups schema field 'users' to 'otherUsers'

--- src/db/models/expenseGroups.ts
+++ src/db/models/expenseGroups.ts
@@ -1,12 +1,21 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
-const expenseGroupSchema = new Schema({
-    admin: { type: mongoose.Types.ObjectId, required: true },
+const expenseGroupSchema = new Schema(
+  {
+    admin: { type: mongoose.Types.ObjectId, ref: "users", required: true },
     name: { type: String, required: true },
     description: String,
-    users: [mongoose.Types.ObjectId],
-    createdBy: { type: mongoose.Types.ObjectId, required: true }
-}, { timestamps: true });
+    otherUsers: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+    createdBy: { type: mongoose.Types.ObjectId, required: true },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.models.expensegroups || mongoose.model("expensegroups", expenseGroupSchema);
+export default mongoose.models.expensegroups ||
+  mongoose.model("expensegroups", expenseGroupSchema);
